208d8d8bd50337d4965b338981decbe00a4911b8
pioucyril <cyril.piou@cirad.fr>
2023-07-27 16:38:26 +0200

    forecasts end of july 2023

--- forecasts.R
+++ forecasts.R
@@ -13,7 +13,7 @@ library(lubridate)
 library(viridis)
 path="D:/Mes Donnees/GitHub/LocustForecastCLCPRO3/"
 startdate="2023-06-01"
-howmanydecades=5
+howmanydecades=6
 gregariousmodel=FALSE
 fieldData=FALSE
 
