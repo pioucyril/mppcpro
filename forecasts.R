3544b3d17021a4dd5656a9f0f4a3981e6e2c8070
Elodie Fernandez <elodie.fernandez@cirad.fr>
2024-07-05 13:08:39 +0000

    add license header

--- forecasts.R
+++ forecasts.R
@@ -1,3 +1,27 @@
+#------------------------------------------------------------------------
+#
+#  This file is part of      MPPCPRO
+#
+#  Model de Prevision de Presence du Criquet Pelerin en Region Occidentale
+#  
+#     Copyright (C) CIRAD - FAO (CLCPRO) 2021 - 2024
+#  
+#  Developped by Lucile Marescot, Elodie Fernandez and Cyril Piou
+#  
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#------------------------------------------------------------------------
+
 ##########################################################################
 #  MPPCPRO file example                                                  #
 #  to get the outputs of the model into an HTML using leaflet            #
